767b15ea2464d74911f7b2b43529543c5f6e6467
Donald Njumbe Ediage <ediage@yahoo.com>
2019-06-22 14:07:28 +0300

    deploy

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className="white f3">{`${name}, you current count is...`}</div>
+      <div className="white f1">{entries}</div>
+    </div>
+  );
+};
+export default Rank;
